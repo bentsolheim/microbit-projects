cd61f8ea7c675fe184785cfe843ab102109f7278
Bent André Solheim <bent.andre.solheim@kodehuset.no>
2019-12-01 01:12:09 +0100

    First iteration of calculator.py

---
+++ README.md
@@ -0,0 +1,19 @@
+
+https://mryslab.github.io/pseudo-microbit/install/
+
+
+
+https://uflash.readthedocs.io/en/latest/
+
+sudo pip install uflash
+
+
+To upgrade:
+(pip install --no-cache --upgrade uflash)
+
+
+pip install pseudo-microbit
+
+pip freeze > requirements.txt
+
+pip install -r requirements.txt
---
+++ calculator.py
@@ -0,0 +1,51 @@
+from microbit import *
+
+tall1 = 1
+tall2 = 1
+modus = 1
+
+newMessage = None
+currentMessage = None
+timePressed = 0
+
+while True:
+
+    if modus == 1:
+        newMessage = str(tall1)
+    if modus == 2:
+        newMessage = str(tall2)
+    if modus == 3:
+        newMessage = str(tall1 * tall2)
+    if modus == 4:
+        tall1 = 1
+        tall2 = 1
+        modus = 1
+
+    if newMessage != currentMessage:
+        print("Write message: " + newMessage)
+        currentMessage = newMessage
+        if modus == 3:
+            display.scroll(currentMessage, wait=False, loop=True)
+        else:
+            display.show(currentMessage, wait=False, loop=True)
+
+    if button_a.is_pressed() == 1:
+        if timePressed == 0:
+            modus = modus + 1
+        timePressed += 25
+    if button_b.is_pressed() == 1:
+        timePressed += 25
+        if timePressed >= 75:
+            if modus == 1:
+                tall1 = tall1 + 1
+                # if tall1 >= 10:
+                #    tall1 = 1
+            if modus == 2:
+                tall2 = tall2 + 1
+                # if tall2 >= 10:
+                #    tall2 = 1
+            timePressed = 0
+
+    if button_a.is_pressed() == 0 and button_b.is_pressed() == 0:
+        timePressed = 0
+    sleep(25)
